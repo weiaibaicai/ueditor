ce779c13cf662851c9947315d317d714feede455
BCai <923108452@qq.com>
2021-12-06 15:08:37 +0800

    alter: 修改发布配置的路径

--- src/UeditorServiceProvider.php
+++ src/UeditorServiceProvider.php
@@ -46,7 +46,7 @@ class UeditorServiceProvider extends ServiceProvider
         });
 
         $this->publishes([
-            __DIR__.'/config/ueditor.php' => config_path('ueditor.php'),
+            __DIR__.'/../config/ueditor.php' => config_path('ueditor.php'),
         ], 'config');
 
         parent::init();
